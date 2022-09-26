d9499b93144249a9db7906ca0a8e77a4b0f4c1a3
HanSeongLee <jkhanseong@gmail.com>
2022-09-26 21:10:45 +0900

    ADD | Error style in Input component

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -3,12 +3,14 @@ import styles from './style.module.scss';
 import cn from 'classnames';
 
 interface IProps extends InputHTMLAttributes<HTMLInputElement> {
-
+    error?: boolean;
 }
 
-const Input: React.FC<IProps> = React.forwardRef<HTMLInputElement, IProps>(({ className, ...props }, ref) => {
+const Input: React.FC<IProps> = React.forwardRef<HTMLInputElement, IProps>(({ error, className, ...props }, ref) => {
     return (
-        <input className={cn(styles.input, className)}
+        <input className={cn(styles.input, className, {
+            [styles.error]: error,
+        })}
                {...props}
                ref={ref}
         />
