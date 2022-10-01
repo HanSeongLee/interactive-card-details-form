06cafa8073bd1a022f98cde47e86ae704aa3a7fd
HanSeongLee <jkhanseong@gmail.com>
2022-10-01 21:30:18 +0900

    ADD | Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from 'next'
+import styles from './style.module.scss';
+import CardInteractionFormContainer from 'containers/CardInteractionFormContainer';
+import Container from 'components/Container';
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
-}
+    return (
+        <>
+            <main className={styles.main}>
+                <Container>
+                    <CardInteractionFormContainer className={styles.cardInteractionFormContainer} />
+                </Container>
+            </main>
+        </>
+    );
+};
 
 export default Home
